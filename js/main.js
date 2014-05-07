98a469bddfa51f4894f507a87261bd794b826896
jandrade <juandavidandrade@gmail.com>
2014-05-06 21:10:59 -0500

    beta page

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * @fileOverview Demos for Slider
+ * @author Juan Andrade <juandavidandrade@gmail.com>
+ * @version 1.0
+ */
+
+/* global bite */
+
+var slider = new bite.Slider('.slider-default', {
+    breakpoints: [
+        { width: 320, items: 1},
+        { width: 480, items: 2},
+        { width: 768, items: 3},
+        { width: 1024, items: 6}
+    ]
+});
+
+var sliderSingle = new bite.Slider('.slider-single', {
+    forceTouch: true,
+    single: true
+});
+
+var sliderAuto = new bite.Slider('.slider-auto', {
+    auto: true,
+    single: true,
+    forceTouch: true,
+    duration: 4000
+});
+
+var sliderNoInf = new bite.Slider('.slider-no-inf', {
+    infinite: false
+});
+
+var sliderVertical = new bite.Slider('.slider-vertical', {
+    forceTouch: true,
+    infinite: false,
+    vertical: true,
+    single: true
+});
