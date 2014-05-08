d22bada61eb67d69a13795033a8f31ac04cca926
jandrade <juandavidandrade@gmail.com>
2014-05-07 21:14:26 -0500

    added DOMContentLoaded example

--- js/main.js
+++ js/main.js
@@ -5,35 +5,36 @@
  */
 
 /* global bite */
+document.addEventListener('DOMContentLoaded', function() {
+    var slider = new bite.Slider('.slider-default', {
+        breakpoints: [
+            { width: 320, items: 1},
+            { width: 480, items: 2},
+            { width: 768, items: 3},
+            { width: 1024, items: 6}
+        ]
+    });
 
-var slider = new bite.Slider('.slider-default', {
-    breakpoints: [
-        { width: 320, items: 1},
-        { width: 480, items: 2},
-        { width: 768, items: 3},
-        { width: 1024, items: 6}
-    ]
-});
+    var sliderSingle = new bite.Slider('.slider-single', {
+        forceTouch: true,
+        single: true
+    });
 
-var sliderSingle = new bite.Slider('.slider-single', {
-    forceTouch: true,
-    single: true
-});
+    var sliderAuto = new bite.Slider('.slider-auto', {
+        auto: true,
+        single: true,
+        forceTouch: true,
+        duration: 4000
+    });
 
-var sliderAuto = new bite.Slider('.slider-auto', {
-    auto: true,
-    single: true,
-    forceTouch: true,
-    duration: 4000
-});
+    var sliderNoInf = new bite.Slider('.slider-no-inf', {
+        infinite: false
+    });
 
-var sliderNoInf = new bite.Slider('.slider-no-inf', {
-    infinite: false
-});
-
-var sliderVertical = new bite.Slider('.slider-vertical', {
-    forceTouch: true,
-    infinite: false,
-    vertical: true,
-    single: true
+    var sliderVertical = new bite.Slider('.slider-vertical', {
+        forceTouch: true,
+        infinite: false,
+        vertical: true,
+        single: true
+    });
 });
